0ae224456f43b185e3e2cb596b533da5cd3650d9
caimingqi <gbafang2021@163.com>
2022-10-14 13:45:21 +0800

    turtle

---
+++ practice-project/Turtlepaint/turtlepaint.py
@@ -0,0 +1,96 @@
+"""
+Turtle模块提供了在二维平面上移动的环境。
+Turtle可以实现位置、航向和各种可能的状态和动作。
+"""
+import turtle as tu
+
+roo = tu.Turtle() # 创建对象
+wn = tu.Screen() # 屏幕对象
+wn.bgcolor("black") # 屏幕背景
+wn.title("标题")
+roo.left(90) # 移动
+roo.speed(20) # 速度
+
+def draw(l,color): # 以长度‘l’作为参数的递归函数
+    if l < 10:
+        return
+    else:
+        roo.pensize(2)
+        roo.pencolor(color)
+        roo.forward(l)
+        roo.left(30)
+        draw(3*l/4,color)
+        roo.right(60)
+        draw(3*l/4,color)
+        roo.left(30)
+        roo.pensize(2)
+        roo.backward(l)
+
+def draw1(l,color): # 以长度‘l’作为参数的递归函数
+    if l < 10:
+        return
+    else:
+        roo.pensize(3)
+        roo.pencolor(color)
+        roo.forward(l)
+        roo.left(30)
+        draw1(4*l/5,color)
+        roo.right(60)
+        draw1(4*l/5,color)
+        roo.left(30)
+        roo.pensize(3)
+        roo.backward(l)
+
+def draw2(l,color): # 以长度‘l’作为参数的递归函数
+    if l < 10:
+        return
+    else:
+        roo.pensize(2)
+        roo.pencolor(color)
+        roo.forward(l)
+        roo.left(30)
+        draw2(6*l/7,color)
+        roo.right(60)
+        draw2(6*l/7,color)
+        roo.left(30)
+        roo.pensize(2)
+        roo.backward(l)  
+
+colors = ["magenta","red",'#FFF8DC',"lightgreen","cyan"]
+colors1 = ["magenta","red",'#FFF8DC',"lightgreen","cyan"]
+colors2 = ["magenta","red",'#FFF8DC',"lightgreen","cyan"]
+
+draw(20,"black")
+roo.right(90)
+roo.speed(2000)
+
+def direction(i):
+    if i % 2 == 0:
+        roo.left(270)
+        roo.speed(2000)
+    else:
+        roo.right(90)
+        roo.speed(2000)
+
+
+def startPrint():
+    i = 1
+    for color in colors:
+        draw(20,color)
+        direction(i)
+        i+=1
+    i = 1
+    for color in colors1:
+        draw1(40,color)
+        direction(i)
+        i+=1
+    i = 1
+    for color in colors2:
+        draw2(60,color)
+        direction(i)
+        i+=1      
+
+    wn.exitonclick()
+
+if __name__ == "__main__":
+    startPrint()
--- practice-project/lifegame/lifegame.py
+++ practice-project/lifegame/lifegame.py
@@ -23,15 +23,29 @@ rows=len(board)
 cols=len(board[0])
 
 # 创建一个原始板的副本
+copy_board = [[board[row][col] for col in range(cols)] for row in range(rows)]
+
+# 逐个单元地迭代
 for row in range(rows):
     for col in range(cols):
 
         # 对于每个单元计算邻居的数量
         live_neighbors = 0
-        for neighbor in range(neighbors):
+        for neighbor in neighbors:
 
             r = (row+neighbor[0])
             c = (col+neighbor[1])
 
             # 检查相邻细胞的有效性，以及它是否原来是一个活细胞
             # 评估是针对副本进行的，因为它永远不会更新。
+            if(r < rows and r >= 0) and (c < cols and c >= 0) and (copy_board[r][c] == 1):
+                live_neighbors += 1
+
+        # 规则1或规则3
+        if copy_board[row][col] == 1 and (live_neighbors < 2 or live_neighbors > 3):
+            board[row][col] = 0
+        # 规则4
+        if copy_board[row][col] == 0 and live_neighbors == 3:
+            board[row][col] = 1
+
+print(board)
